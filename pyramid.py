7705497518e7cdd63a9aa3c6d4f2c032679b73fe
Winfried Loetzsch <winfried.loetzsch@student.hpi.de>
2021-04-29 18:41:49 +0200

    experiment with threshold. probably wrong dt filter?

--- pyramid.py
+++ pyramid.py
@@ -9,15 +9,20 @@ def pyramid(image):
     ds = []
     dhats = []
     fs = []
-    k = 5
+    k = 4
 
     for i in range(k):
-        sigma_c = 1 if i < k - 1 else 250
-        sigma_s = math.pow(2, i+2)
+        sigma_c = 1.0  # 0.1?
+        sigma_s = math.pow(2, i + 2)
 
-        r = cv2.ximgproc.dtFilter(image[:, :, 0], image[:, :, 0], sigma_s, sigma_c, numIters=5)
-        g = cv2.ximgproc.dtFilter(image[:, :, 1], image[:, :, 0], sigma_s, sigma_c, numIters=5)
-        b = cv2.ximgproc.dtFilter(image[:, :, 2], image[:, :, 0], sigma_s, sigma_c, numIters=5)
+        if i == k - 1:
+            r = cv2.GaussianBlur(image[:, :, 0], (0, 0), sigma_s)
+            g = cv2.GaussianBlur(image[:, :, 1], (0, 0), sigma_s)
+            b = cv2.GaussianBlur(image[:, :, 2], (0, 0), sigma_s)
+        else:
+            r = cv2.ximgproc.dtFilter(image[:, :, 0], image[:, :, 0], sigma_s, sigma_c, numIters=5)
+            g = cv2.ximgproc.dtFilter(image[:, :, 1], image[:, :, 0], sigma_s, sigma_c, numIters=5)
+            b = cv2.ximgproc.dtFilter(image[:, :, 2], image[:, :, 0], sigma_s, sigma_c, numIters=5)
 
         if i == 0:
             last_r = image[:, :, 0]
@@ -28,8 +33,8 @@ def pyramid(image):
             last_g = lps[-1][1]
             last_b = lps[-1][2]
 
-        d = np.abs(r-last_r) + np.abs(g-last_g) + np.abs(b-last_b)
-        dhat = cv2.ximgproc.dtFilter(image, d, 50.0, 25.0, numIters=5)
+        d = np.sqrt(np.square(r - last_r) + np.square(g - last_g) + np.square(b - last_b))
+        dhat = cv2.ximgproc.dtFilter(image, d, 20.0, sigma_c, numIters=5)
         # dhat[dhat < 0.4] = 0.0
 
         lps.append((r, g, b))
@@ -37,10 +42,12 @@ def pyramid(image):
         dhats.append(dhat)
 
     m = np.ones_like(ds[0])
-    for i in range(k-3, -1, -1):
-        f = dhats[i] * (m * (dhats[i] > dhats[i+1]))
+    for i in range(k - 1):
+        thresh = (1.0 - i / (k - 3)) * 0.04
+
+        f = dhats[i] * ((dhats[i] > thresh) * m * (dhats[i] > dhats[i + 1]))
         m = m * (f == 0.0)
-        f = dhats[i] > dhats[i+1]
+
         fs.append(f)
 
     return lps, ds, dhats, fs
--- rank_image.py
+++ rank_image.py
@@ -5,15 +5,16 @@ from pyramid import pyramid
 
 
 def main():
-    img = np.array(Image.open("test_image.png").convert("RGB")) / 255.0
-    f, ax = plt.subplots(6, figsize=(8, 20))
+    img = np.array(Image.open("img2.jpeg").convert("RGB")) / 255.0
+    _, _, _, p = pyramid(img)
+
+    f, ax = plt.subplots(len(p) + 1, figsize=(8, 20))
     ax[0].imshow(img)
-    _, p, _, rr = pyramid(img)
 
     ax[0].imshow(img)
     ax[0].axis("off")
 
-    for i in range(3):
+    for i in range(len(p)):
         ax[i+1].imshow(p[i])
         ax[i+1].axis("off")
 
